a8a7201b0eb936651b9741e1600dd47792f099c5
Ciocca Vincent <ciocca.vincent@outlook.fr>
2023-04-12 12:54:32 +0200

    affichage des couleurs

--- front/js/index.js
+++ front/js/index.js
@@ -1,23 +1,28 @@
-fetch("http://localhost:3000/api/products/")
-.then(res => res.json())
+async function displayKanaps(){
+    await fetch("http://localhost:3000/api/products/") // conextion avec l'API
+    .then(res => res.json())
 
-.then(function(value){
-    for(let i = 0; i < value.length; i++){
-       document.querySelector(".items").innerHTML +=
-                `<a href="./product.html?id=${value[i]._id}">
-                    <article>
-                        <img src=${value[i].imageUrl} alt=${value[i].altTxt}>
-                        <h3 class="productName">${value[i].name}</h3>
-                        <p class="productColors">${value[i].colors}</p>
-                        <p class="productDescription">${value[i].description}</p>
-                        <p class="productPrice">${value[i].price}€</p>
-                    </article>
-                 </a>`
-                 ;
-                }})
+    .then(function(value){
+        for(let i = 0; i < value.length; i++){ //boucle pour afficher tous les kanap de la base de donner
+        document.querySelector(".items").innerHTML += //conextion avec le HTML
+                    `<a href="./product.html?id=${value[i]._id}">
+                        <article>
+                            <img src=${value[i].imageUrl} alt=${value[i].altTxt}>
+                            <h3 class="productName">${value[i].name}</h3>
+                            <p class="productColors">${value[i].colors}</p>
+                            <p class="productDescription">${value[i].description}</p>
+                            <p class="productPrice">${value[i].price}€</p>
+                        </article>
+                    </a>` //le code concatener qu'on envoie dans le HTML
+                    ;
+                    }})
 
-.catch(function(error){
-    console.log('Il y a un problème avec le fetch : ' + error.message);
-});
+    .catch(function(error){
+        console.log('Il y a un problème avec le fetch : ' + error.message);
+    });
+}
+displayKanaps()
+
+// async function permet de structurer le code et de le rendre plus rapide lors du chargement
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -1,35 +1,50 @@
-let params = new URLSearchParams(document.location.search);
-let id = params.get ("id");
-console.log(id);
-
-fetch("http://localhost:3000/api/products/"+ id)
-.then(res => res.json())
-.then(function(value){
-    console.log(value);
-       document.querySelector(".item__img").innerHTML +=
-                `<img src=${value.imageUrl} alt=${value.altTxt}>`;
-
-        document.getElementById("title").innerHTML +=
-         `${value.name}`;
-
-        document.getElementById("price").innerHTML +=
-         `${value.price}`;
-
-        document.querySelector(".item__content__description").innerHTML +=
-                `<p id="description">${value.description}</p>`;
-        })
-// .then (function(Colors){
-//     console.log(Colors);
-// for(let i = 0; i < colors.length; i++){
-    
-//     document.getElementById("colors").innerHTML +=
-//         `<option value="${Colors[i].colors}">${Colors[i].colors}</option>`;
-//                     }})
+async function displayKanap(){
+        // récuperer l'id du canaper selectionner
+    let params = new URLSearchParams(document.location.search);
+    let id = params.get ("id");
+    console.log(id);
 
 
+    await fetch("http://localhost:3000/api/products/"+ id) // intégrer l'id du canaper selectionner dans le fetch "+ id"
+    .then(res => res.json())
 
+    .then(function(value){
+        console.log(value);
+
+            document.querySelector(".item__img").innerHTML += // affiche l'image stocker dans le models dans le code HTML via la classe
+                    `<img src=${value.imageUrl} alt=${value.altTxt}>`;
+
+            document.getElementById("title").innerHTML += // affiche le nom stocker dans le models dans le code HTML via l'id
+            `${value.name}`;
+
+            document.getElementById("price").innerHTML += // affiche le prix stocker dans le models dans le code HTML via l'id
+            `${value.price}`;
+
+            document.querySelector(".item__content__description").innerHTML += // affiche la description stocker dans le models dans le code HTML via la classe
+                    `<p id="description">${value.description}</p>`;
+
+            for(let color of value.colors){ //Boucle pour eviter une répetition de code et avoir chaque couleur lier a chaque canaper
         
+                document.getElementById("colors").innerHTML += // affiche les couleurs pour un canaper dans le selecteur HTML via l'id
+                    `<option value="${color}">${color}</option>`;
+                }
 
-.catch(function(error){
-    console.log('ALERT : ' + error.message);
-    });
+            // document.getElementById("colors").innerHTML +=
+            //         `<option value=${value.colors[0]}>${value.colors[0]}</option>
+            //         <option value=${value.colors[1]}>${value.colors[1]}</option>
+            //         <option value=${value.colors[2]}>${value.colors[2]}</option>`;
+            })
+
+    .catch(function(error){
+        console.log('ALERT : ' + error.message);
+        });
+}
+displayKanap() //permet le bon fonctionnement de la function
+
+
+
+// function sectionKanap(){
+//     document.getElementById('addToCart').click();
+//     addEventListener()
+// }
+// sectionKanap()
